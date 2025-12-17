b4a3728ef6690934348b8ac0ed8fd2be9cd99035
Mohammed_Bahr <mohammedbahr686@gmail.com>
2025-12-17 10:04:16 +0200

    621. Task Scheduler

--- Solutions/621. Task Scheduler.cpp
+++ Solutions/621. Task Scheduler.cpp
@@ -2,44 +2,39 @@
 using namespace std;
 
 class Solution {
-    public:
-        int leastInterval(vector<char>& tasks, int n) {
-          map<char , pair<int , int>> mp;
-          for (auto i: tasks) {
-            mp[i].first++;
-          }
-          int time = 0;
-          while (true) {
-            int maxi = 0;
-            char task = ' ';
-            for (auto i: mp) {
-              if (i.second.first > maxi && i.second.second <= time) {
-                maxi = i.second.first;
-                task = i.first;
-              }
-            }
-            if (task == ' ') {
-              break;
-            }
-            mp[task].first--;
-            mp[task].second = time + n + 1;
-            time++;
-          }
-          return time;
-            
-        }
-    };
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> freq(26, 0);
 
-int main() {
-    string s = "abc";
-    string t = "ahbgdc";
-    vector <int> nums = {1,12,-5,-6,50,3};
-    int k = 4;
-    vector <char> tasks = {'A','A','A','B','B','B'};
-    int n = 2;
+        for (char c : tasks)
+            freq[c - 'A']++;
 
-    Solution S; 
-    cout << S.leastInterval(tasks, n);
+        int maxFreq = *max_element(freq.begin(), freq.end());
 
-    return 0;
+        int countMax = 0;
+        for (int f : freq)
+            if (f == maxFreq)
+                countMax++;
+
+        int totalTasks = tasks.size();
+
+        int result = (maxFreq - 1) * (n + 1) + countMax;
+
+        return max(totalTasks, result);
+    }
+};
+
+int main()
+{
+  string s = "abc";
+  string t = "ahbgdc";
+  vector<int> nums = {1, 12, -5, -6, 50, 3};
+  int k = 4;
+  vector<char> tasks = {'A', 'A', 'A', 'B', 'B', 'B'};
+  int n = 2;
+
+  Solution S;
+  cout << S.leastInterval(tasks, n);
+
+  return 0;
 }
